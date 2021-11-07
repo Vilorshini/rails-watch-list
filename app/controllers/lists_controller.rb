3c5f4b5a1b0ffc3dd74312c6d26c5fcf734e5600
vilorshini ramsamy <vilorshiniramsamy02@gmail.com>
2021-11-07 21:36:31 +0400

    watch list front end

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
-
   before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
@@ -19,12 +19,14 @@ class ListsController < ApplicationController
   end
 
   def show
+    
     @bookmark = Bookmark.new
   end
 
-   def destroy
-    @list.destroyredirect_to lists_path
-   end
+  def destroy
+  @list.destroy 
+  redirect_to lists_path
+  end
 
   private
 
@@ -35,6 +37,4 @@ class ListsController < ApplicationController
   def list_params
     params.require(:list).permit(:name)
   end
-
-
 end
